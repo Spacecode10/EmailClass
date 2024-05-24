a23c48a164dde4b2c07684104c0209e2bb89803f
dhyana08 <dhyana0810@gmail.com>
2024-05-24 15:48:42 +0530

    orm db

--- EmailClassifier/Email/views.py
+++ EmailClassifier/Email/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from transformers import AutoModelForSequenceClassification
+from transformers import BertForSequenceClassification,BertTokenizerFast
+from django.http import JsonResponse, HttpResponse
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 # Create your views here.
 def index(request):
-    if request.method=="POST":
-        return render(request, "classify.html")
+    if request.method=="post":
+        print(request.POST)
+    return render(request, "classify.html")
+
+def result(request):
+    email=request.POST.get("email")
+    model = AutoModelForSequenceClassification.from_pretrained('E:\project\model-params')
+    tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased", max_length=512)
+    inputs = tokenizer(email, padding=True, truncation=True, max_length=512, return_tensors="pt")
+    outputs = model(**inputs)
+    probs = outputs[0].softmax(1)
+    pred_label_idx = probs.argmax()
+    pred_label = model.config.id2label[pred_label_idx.item()]
+    # return pred_label
+    print(pred_label)
+    return render(request, "result.html",{'result':pred_label})
